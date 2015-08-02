9675347aa6009ed7045b1a80695ca42bd9e298a6
mEDI <medi@255.li>
2015-08-02 22:47:51 +0200

    cleanup

--- find_A-B-A_route.py
+++ find_A-B-A_route.py
@@ -17,14 +17,14 @@ search best deal in my system for a A-B-A route
 
 startSystem = "ltt 9810"
 
-searchModus = 2         # 1=bestDealFromMySystem 2=findBestDealInMyCircle
-maxDist = 40            # max distace for B system
-maxSearchRange = 80    # only used in mode 2
+searchModus = 2  # 1=bestDealFromMySystem 2=findBestDealInMyCircle
+maxDist = 40  # max distace for B system
+maxSearchRange = 60  # only used in mode 2
 maxStarDist = 1300
-maxAge = 14             # max data age in days
+maxAge = 14  # max data age in days
 minLoopProfit = 3000
-minTradeProfit = 1000    # only to minimize results (speedup)
-minStock = 150000        # > 10000 = stable route > 50000 = extrem stable route and faster results
+minTradeProfit = 1000  # only to minimize results (speedup)
+minStock = 150000  # > 10000 = stable route > 50000 = extrem stable route and faster results
 
 maxAgeDate = datetime.utcnow() - timedelta(days=maxAge)
 
@@ -38,13 +38,13 @@ def findDealsFromTo(pricesListA, pricesListB):
     dealItems = {}
     for itemA in pricesListA:
         if itemA["StationSell"] > 0 and itemA["Stock"] > minStock:
-            #find buy station deal
+            # find buy station deal
             for itemB in pricesListB:
                 if itemA["ItemID"] is itemB["ItemID"]:
-                    #same item
+                    # same item
                     profitAtoB = itemB["StationBuy"] - itemA["StationSell"]
                     if profitAtoB > 0 and profitAtoB > minTradeProfit:
-                        #print(profitAtoB,itemA, "and", itemB)
+                        # print(profitAtoB,itemA, "and", itemB)
                         if itemA["SystemID"] not in dealItems:
                             dealItems[itemA["SystemID"]] = {}
                         if itemB["StationID"] not in dealItems[itemA["SystemID"]]:
@@ -56,56 +56,31 @@ def findDealsFromTo(pricesListA, pricesListB):
                         if itemA["ItemID"] not in dealItems[itemA["SystemID"]][itemB["StationID"]][itemA["StationID"]]: 
                             dealItems[itemA["SystemID"]][itemB["StationID"]][itemA["StationID"]][itemA["ItemID"]] = []
 
-                        dealItems[itemA["SystemID"]][itemB["StationID"]][itemA["StationID"]][itemA["ItemID"]] = [profitAtoB,itemA,itemB] 
+                        dealItems[itemA["SystemID"]][itemB["StationID"]][itemA["StationID"]][itemA["ItemID"]] = [profitAtoB, itemA, itemB] 
     return dealItems
 
 if searchModus is 1:
-    pricesListA = mydb.getPricesFrom(startSystem,maxStarDist ,maxAgeDate)
-    pricesListB = mydb.getPricesInDistance( startSystem, maxDist, maxStarDist, maxAgeDate)
-    dealsListBtoA = findDealsFromTo(pricesListA,pricesListB )
-    dealsListAtoB = findDealsFromTo(pricesListB,pricesListA ) 
-#    dealsListBtoA = sorted(dealsListBtoA , key=lambda deal: deal[0],reverse=True )
-#    dealsListAtoB = sorted(dealsListAtoB , key=lambda deal: deal[0],reverse=True )
+    pricesListA = mydb.getPricesFrom(startSystem, maxStarDist , maxAgeDate)
+    pricesListB = mydb.getPricesInDistance(startSystem, maxDist, maxStarDist, maxAgeDate)
+    dealsListBtoA = findDealsFromTo(pricesListA, pricesListB)
+    dealsListAtoB = findDealsFromTo(pricesListB, pricesListA) 
 elif searchModus is 2:
     start2 = timeit.default_timer()
 
-    pricesListA = mydb.getPricesInDistance( startSystem, maxSearchRange, maxStarDist, maxAgeDate)
+    pricesListA = mydb.getPricesInDistance(startSystem, maxSearchRange, maxStarDist, maxAgeDate)
 
-    stop = timeit.default_timer()
-    print("getPricesInDistance",round(stop - start2, 3))
+    print("getPricesInDistance", round(timeit.default_timer() - start2, 3))
+
     start2 = timeit.default_timer()
 
-    dealsListBtoA = findDealsFromTo(pricesListA,pricesListA )
+    dealsListBtoA = findDealsFromTo(pricesListA, pricesListA)
 
-    stop = timeit.default_timer()
-    print("findDealsFromTo",round(stop - start2, 3))
+    print("findDealsFromTo", round(timeit.default_timer() - start2, 3))
 
-#    dealsListAtoB,dealsListAtoB2 = findDealsFromTo(pricesListA,pricesListA )
-
-    #dealsListBtoA = sorted(dealsListBtoA , key=lambda deal: deal[0],reverse=True )
     dealsListAtoB = dealsListBtoA
 
-bestProfitList = []
-def findTradingLoop_old(dealsListAtoB,dealsListBtoA):
-    '''
-    search a back loop deal with profiet
-    '''
-    for dealBtoAprofit,dealBtoA,dealBtoA_A in dealsListBtoA:
-        #print(dealAtoB["SystemID"], systemID)
-        #print(dealBtoAprofit,dealBtoA,dealBtoA_A)
-        #find best back deal from system->station and calc total profit
-        for dealAtoBprofit,dealAtoB,dealAtoB_A in dealsListAtoB:
-            #print(dealBtoA["StationID"], dealAtoB["StationID"])
-            if dealAtoB["StationID"] == dealBtoA_A["StationID"] and dealBtoA["StationID"] == dealAtoB_A["StationID"] : # StationBtoA:
-                #stationDone[dealBtoA["StationID"]] = True
-                #print(dealBtoAprofit+ dealAtoBprofit,dealBtoA,dealAtoB)
-                profit = dealBtoAprofit + dealAtoBprofit
-                if profit > minLoopProfit:
-                    bestProfitList.append( [ profit ,dealBtoA,dealBtoA_A, dealAtoB,dealAtoB_A ] )
-                break
-    return bestProfitList
 
-def findTradingLoop(dealsListAtoB,dealsListBtoA):
+def findTradingLoop(dealsListAtoB, dealsListBtoA):
     '''
     search a back loop deal with profiet
 
@@ -118,7 +93,7 @@ def findTradingLoop(dealsListAtoB,dealsListBtoA):
         for stationID_B in dealsListAtoB[systemID]:
             for stationID_A in dealsListAtoB[systemID][stationID_B]:
                 for itemA_ID in dealsListAtoB[systemID][stationID_B][stationID_A]:
-            #search back part
+            # search back part
                     for systemID_B in dealsListBtoA:
                         if dealsListBtoA[systemID_B].get(stationID_A) and dealsListBtoA[systemID_B][stationID_A].get(stationID_B):
                             for itemB_ID in dealsListBtoA[systemID_B][stationID_A][stationID_B]:
@@ -126,54 +101,51 @@ def findTradingLoop(dealsListAtoB,dealsListBtoA):
                 
                                 profit = dealsListAtoB[systemID][stationID_B][stationID_A][itemA_ID][0] + dealsListBtoA[systemID_B][stationID_A][stationID_B][itemB_ID][0]
                                 if profit > minLoopProfit:
-                                    bestProfitList.append( [ profit , dealsListAtoB[systemID][stationID_B][stationID_A][itemA_ID][1] , dealsListAtoB[systemID][stationID_B][stationID_A][itemA_ID][2], dealsListBtoA[systemID_B][stationID_A][stationID_B][itemB_ID][1], dealsListBtoA[systemID_B][stationID_A][stationID_B][itemB_ID][2] ] )
+                                    bestProfitList.append([ profit , dealsListAtoB[systemID][stationID_B][stationID_A][itemA_ID][1] , dealsListAtoB[systemID][stationID_B][stationID_A][itemA_ID][2], dealsListBtoA[systemID_B][stationID_A][stationID_B][itemB_ID][1], dealsListBtoA[systemID_B][stationID_A][stationID_B][itemB_ID][2] ])
 
                 
     return bestProfitList
 
 start2 = timeit.default_timer()
 
-bestProfitList = findTradingLoop(dealsListAtoB,dealsListBtoA)
+bestProfitList = findTradingLoop(dealsListAtoB, dealsListBtoA)
 
-print("findTradingLoop",round(timeit.default_timer() - start2, 3))
+print("findTradingLoop", round(timeit.default_timer() - start2, 3))
 
-#sys.exit()
-#bestProfitList = findTradingLoop(dealsListAtoB,dealsListBtoA)
 print("results", len(bestProfitList))
 
-## remove dubel entrys in mode 2
+# # remove dubel entrys in mode 2
 for deal in bestProfitList:
     for dealB  in bestProfitList[bestProfitList.index(deal):]:
         if deal[1]["SystemID"] is dealB[3]["SystemID"] and deal[1]["StationID"] is dealB[3]["StationID"] and deal[1]["ItemID"] is dealB[3]["ItemID"]:
             bestProfitList.remove(dealB)
             break
-#            print("hit") 
-        pass
+
 print("results after cleaning", len(bestProfitList))
 
 '''
 calculate a rating "only profit is not the best option on long ways"
 '''
 for deal in bestProfitList:
-    startDist = (deal[1]["StarDist"]+deal[3]["StarDist"])
+    startDist = (deal[1]["StarDist"] + deal[3]["StarDist"])
    # rating = deal[0] - startDist/1.1
-    rating = deal[0] +  minLoopProfit /  (startDist*0.015)
+    rating = deal[0] + minLoopProfit / (startDist * 0.03)
 #    print(rating, deal[0], startDist)
     deal.append(rating)
 
 '''
 print results (best deals list)
 '''
-bestProfitList = sorted(bestProfitList , key=lambda deal: deal[5],reverse=True )
+bestProfitList = sorted(bestProfitList , key=lambda deal: deal[5], reverse=True)
 count = 0
 for deal in bestProfitList:
 
     if searchModus is 1:
-        dist = round(deal[1]["dist"],2)
+        dist = round(deal[1]["dist"], 2)
     elif searchModus is 2:
-        dist = round(mydb.getDistanceFromTo(deal[1]["SystemID"], deal[2]["SystemID"]),2)
+        dist = round(mydb.getDistanceFromTo(deal[1]["SystemID"], deal[2]["SystemID"]), 2)
     else:
-        dist= -1
+        dist = -1
 
     if deal[1]["StarDist"] > maxStarDist or deal[2]["StarDist"] > maxStarDist or dist > maxDist:
         continue
@@ -181,21 +153,14 @@ for deal in bestProfitList:
     if count > 20:
          break
 
-    print("%d. profit %d dist %s ly rating: %d" % (count, deal[0], dist, deal[5] ) )
-    #print(deal[1])
-    #print(deal[2])
-    #print(deal[3])
-    #print(deal[4])
+    print("%d. profit %d dist %s ly rating: %d" % (count, deal[0], dist, deal[5]))
 
     item = deal[1]
     profit = deal[2]["StationBuy"] - deal[1]["StationSell"]
-    print("\t%s -> %d ls %s buy: %s %d cr (sell for: %d profit: %d cr) " % (item["System"],item["StarDist"],item["Station"],item["name"],deal[1]["StationSell"],deal[2]["StationBuy"],profit ) )
+    print("\t%s -> %d ls %s buy: %s %d cr (sell for: %d profit: %d cr) " % (item["System"], item["StarDist"], item["Station"], item["name"], deal[1]["StationSell"], deal[2]["StationBuy"], profit))
     item = deal[3]
     profit = deal[4]["StationBuy"] - deal[3]["StationSell"]
-    print("\t%s -> %d ls %s buy: %s %d cr (sell for: %d profit: %d cr) " % (item["System"],item["StarDist"],item["Station"],item["name"],deal[3]["StationSell"],deal[4]["StationBuy"],profit ) )
-
-
-
+    print("\t%s -> %d ls %s buy: %s %d cr (sell for: %d profit: %d cr) " % (item["System"], item["StarDist"], item["Station"], item["name"], deal[3]["StationSell"], deal[4]["StationBuy"], profit))
 
 
 
