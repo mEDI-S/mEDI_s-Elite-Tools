39975af71ec1a743bb6f3769de0eff9ca1058bb9
mEDI <medi@255.li>
2015-08-12 03:25:51 +0200

    round distance result now

--- sqlite3_functions.py
+++ sqlite3_functions.py
@@ -7,7 +7,7 @@ def registerSQLiteFunktions(con):
     def calcDistance(AposX, AposY, AposZ, BposX, BposY, BposZ ):
         # #sqrt( (xA-xB)2 + (yA-yB)2 + (zA-zB)2 )
 
-        return math.sqrt((AposX - BposX) ** 2 + (AposY - BposY) ** 2 + (AposZ - BposZ) ** 2)
+        return round(math.sqrt((AposX - BposX) ** 2 + (AposY - BposY) ** 2 + (AposZ - BposZ) ** 2), 2)
 
 
     con.create_function("calcDistance", 6, calcDistance)
