426a97be184d8b69d95b111b51eb39a2f8aa9fb2
mEDI <medi@255.li>
2015-09-14 03:23:42 +0200

    extract calcDistance to external usage

--- sqlite3_functions.py
+++ sqlite3_functions.py
@@ -2,12 +2,10 @@
 
 import math
 
+def calcDistance(AposX, AposY, AposZ, BposX, BposY, BposZ ):
+    # #sqrt( (xA-xB)2 + (yA-yB)2 + (zA-zB)2 )
+    return round(math.sqrt((AposX - BposX) ** 2 + (AposY - BposY) ** 2 + (AposZ - BposZ) ** 2), 2)
+
+
 def registerSQLiteFunktions(con):
-    
-    def calcDistance(AposX, AposY, AposZ, BposX, BposY, BposZ ):
-        # #sqrt( (xA-xB)2 + (yA-yB)2 + (zA-zB)2 )
-
-        return round(math.sqrt((AposX - BposX) ** 2 + (AposY - BposY) ** 2 + (AposZ - BposZ) ** 2), 2)
-
-
     con.create_function("calcDistance", 6, calcDistance)
