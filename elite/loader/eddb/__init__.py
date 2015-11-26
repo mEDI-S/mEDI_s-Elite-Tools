4870a68c87ee1ec80bd0d927474d7989ea6174ee
mEDI <medi@255.li>
2015-11-26 15:21:33 +0100

    disabele eddb loader on exception

--- elite/loader/eddb/__init__.py
+++ elite/loader/eddb/__init__.py
@@ -4,6 +4,7 @@ from elite.loader.eddb import apiv4
 
 from datetime import datetime, timedelta
 import random
+import traceback
 
 
 def updateAll(mydb):
@@ -23,8 +24,13 @@ def updateAll(mydb):
 
     print("update from eddb")
 
-    myapiv4 = apiv4.loader(mydb)
-    myapiv4.importData()
-    del myapiv4
-
+    try:
+        myapiv4 = apiv4.loader(mydb)
+        myapiv4.importData()
+        del myapiv4
+    except:
+        traceback.print_exc()
+        mydb.setConfig('plugin_eddb', 0)
+        print("disable eddb loader!")
+    
     mydb.setConfig( 'lastEDDBimport', datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S") )
